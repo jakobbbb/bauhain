eed7b9ae2684183f284e80c33a621b0022fc3ed8
Jakob Ruckel <jakob@ruckel.de>
2025-04-12 00:16:53 +0200

    GameManager: Add time

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,10 +4,24 @@ public class GameManager : MonoBehaviour {
 
     public static GameManager Instance;
 
+    private float m_GameTimeMinutes = 0;
+
     void Start() {
         if (Instance) {
             DestroyImmediate(Instance);
         }
         Instance = this;
+    }
+
+    public void AdvanceTime(float minutes) {
+        m_GameTimeMinutes += minutes;
+        m_GameTimeMinutes = m_GameTimeMinutes % (60 * 24);
+    }
+
+
+    public string GetFormattedTime() {
+        int h = (int)m_GameTimeMinutes / 60;
+        int m = (int)m_GameTimeMinutes % 60;
+        return h.ToString("D2") + ":" + m.ToString("D2");
     }
 }
